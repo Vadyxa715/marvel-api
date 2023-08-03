1e306dedf6cfd5f8babbdd8918b6a125f3b469e7
Vadyxa715 <vadim_kurdin@mail.ru>
2023-08-03 23:17:47 +0300

    clean start

--- src/main/java/com/kurdev/marvel/MarvelApplication.java
+++ src/main/java/com/kurdev/marvel/MarvelApplication.java
@@ -2,24 +2,11 @@ package com.kurdev.marvel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.kurdev"})
-//@EnableSwagger2
-//@EnableWebMvc
+//@ComponentScan(basePackages = {"com.kurdev"})
 public class MarvelApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MarvelApplication.class, args);
     }
-//    public Docket apis() {
-////        return new Docket(DocumentationType.SWAGGER_2);
-//        return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.basePackage("com.kurdev")).build();
-//    }
 }
